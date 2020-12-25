74b980274637c308a788dec21e52cb29ff83d4fa
shunak <shuna966@gmail.com>
2020-12-25 19:17:25 +0900

    implement login component

--- src/features/login/Login.js
+++ src/features/login/Login.js
@@ -1,7 +1,75 @@
-import React from 'react'
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { useSelector, useDispatch } from "react-redux";
+import styles from "./Login.module.css";
 
+import {
+  editUsername,
+  editPassword,
+  toggleMode,
+  fetchAsyncLogin,
+  fetchAsyncRegster,
+  selectAuthen,
+  selectIsLoginView,
+} from "./loginSlice";
+
+// Login functional component
 const Login = () => {
-    return <div>Hello Login</div>
-}
+  const dispatch = useDispatch();
+  const authen = useSelector(selectAuthen);
+  const isLoginView = useSelector(selectIsLoginView);
+  const btnDisabler = authen.username === "" || authen.password === "";
 
-export default Login
+  const login = async () => {
+    if (isLoginView) {
+      await dispatch(fetchAsyncLogin(authen));
+    } else {
+      const result = await dispatch(fetchAsyncRegster(authen));
+
+      if (fetchAsyncRegster.fulfilled.match(result)) {
+        await dispatch(fetchAsyncLogin(authen));
+      }
+    }
+  };
+
+  return (
+    <div className={styles.containerLogin}>
+      <div className={styles.appLogin}>
+        <h1>{isLoginView ? "Login" : "Register"}</h1>
+        <span>Username</span>
+        <input
+          type="text"
+          className={styles.inputLog}
+          name="username"
+          placeholder=""
+          onChange={(e) => dispatch(editUsername(e.target.value))}
+          required
+        />
+        <span>Password</span>
+        <input
+          type="password"
+          className={styles.inputLog}
+          name="password"
+          placeholder=""
+          onChange={(e) => dispatch(editPassword(e.target.value))}
+          required
+        />
+        <div className={styles.switch}>
+          <Button
+            variant="contained"
+            disabled={btnDisabler}
+            color="primary"
+            onClick={login}
+          >
+            {isLoginView ? "Login" : "Create"}
+          </Button>
+        </div>
+        <span className={styles.switch} onClick={() => dispatch(toggleMode())}>
+          {isLoginView ? "Create Account ?" : "Back to Login"}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
